74c9ba249efbaec06e6dfcce1dde22e444b07907
Robbie <txt3rob@gmail.com>
2015-07-22 10:27:03 +0100

    Update reader.py
    added published date

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 import feedparser
 d = feedparser.parse('http://www.liverpoolecho.co.uk/sport/?service=rss')
 for post in d.entries:
-    print post.title + ": " + post.link + "\n"
+    print post.title + ": " + post.link + "\n" + post.published  + "\n"
+	  
